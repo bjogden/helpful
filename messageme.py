cd6f73ab5abc95f80bae0b7636981a210085c65a
bjogden <bjogden123@gmail.com>
2015-05-08 12:39:34 -0700

    added messageme

---
+++ messageme.py
@@ -0,0 +1,25 @@
+__author__ = 'Bryce Ogden'
+
+from googlevoice import Voice
+from googlevoice.util import input
+
+def message_me(text, **kwargs):
+    """ Send me a message """
+    output = None
+    try:
+        voice = Voice()
+        voice.login(email, password)
+        phone = "phone-number-here"
+
+        if kwargs is not None:
+            for key, value in kwargs.iteritems():
+                text += " | %s: %s" % (key, value)
+
+        voice.send_sms(phone, text)
+        output = "Sucessfully sent message"
+
+    except Exception as e:
+        output = "Error sending message: %s" % e
+
+    finally:
+        print output
